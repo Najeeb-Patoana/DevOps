846c2795e7fbda560a33381d4c12433bb2ea00ea
Najeeb Patoana <najeebpatoana007@gmail.com>
2025-10-14 20:21:28 +0500

    working

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello!' });
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
 });
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'DevOps' });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
